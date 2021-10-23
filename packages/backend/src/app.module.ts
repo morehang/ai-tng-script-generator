8608889e901a6f62af03cb7cb8f77a63bc4017d2
morehang <166088095+morehang@users.noreply.github.com>
2021-10-23 18:07:58 +0000

    v2.1.4

--- packages/backend/src/app.module.ts
+++ packages/backend/src/app.module.ts
@@ -1,3 +1,7 @@
 import { Module, OnModuleInit } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
-import { ConfigModule } from './config/config.module
+import { ConfigModule } from './config/config.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
+import { MongoConnectionService } from './shared/mongo-connection.service';
+import { ScriptGeneratorModule } from
