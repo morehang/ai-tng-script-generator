0e749e4572a1bf7d40e675a9cbe8e649aeab1dc9
morehang <166088095+morehang@users.noreply.github.com>
2021-11-10 19:28:00 +0000

    net: qmi_wwan: fix Oops while disconnecting

--- packages/backend/src/auth/auth/jwt.strategy.ts
+++ packages/backend/src/auth/auth/jwt.strategy.ts
@@ -1,1 +1,7 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '../../config/config.service';
+
+/**
+ * Passport strategy for JWT.
