3b09d8136aab74f6142f46f37f8076a8d435e24e
morehang <166088095+morehang@users.noreply.github.com>
2021-11-28 16:28:12 +0000

    Third

--- packages/backend/src/script-generator/disclaimer.const.ts
+++ packages/backend/src/script-generator/disclaimer.const.ts
@@ -1,4 +1,6 @@
 /**
  * Disclaimer text for generated scripts.
  */
-export c
+export const DISCLAIMER = `DISCLAIMER
+
+"Star Trek", "Star Trek: The Next Generation" and all related Star Trek related material may be or are registered trademarks of Paramount Studi
