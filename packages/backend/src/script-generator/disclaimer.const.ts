a024aebb62b2b9f30dd6b63278b397a6e7a2e3d5
morehang <166088095+morehang@users.noreply.github.com>
2021-11-28 21:42:40 +0000

    0.5.13

--- packages/backend/src/script-generator/disclaimer.const.ts
+++ packages/backend/src/script-generator/disclaimer.const.ts
@@ -3,4 +3,6 @@
  */
 export const DISCLAIMER = `DISCLAIMER
 
-"Star Trek", "Star Trek: The Next Generation" and all related Star Trek related material may be or are registered trademarks of Paramount Studi
+"Star Trek", "Star Trek: The Next Generation" and all related Star Trek related material may be or are registered trademarks of Paramount Studios and CBS.
+
+----`;
---
+++ packages/backend/src/script-generator/script-generator.controller.ts
@@ -0,0 +1,3 @@
+import { Controller, UseGuards, Get, Post, Body, Param, Req } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { ScriptGeneratorService } from './script-generator.servi
