bd744c2185bd16b812a7c77c1a4db2207d07eba3
morehang <166088095+morehang@users.noreply.github.com>
2021-12-09 05:34:56 +0000

    Remove import.

--- packages/backend/src/script-generator/script-generator.controller.ts
+++ packages/backend/src/script-generator/script-generator.controller.ts
@@ -1,3 +1,12 @@
 import { Controller, UseGuards, Get, Post, Body, Param, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
-import { ScriptGeneratorService } from './script-generator.servi
+import { ScriptGeneratorService } from './script-generator.service';
+import { ScriptRepository } from './script.repository';
+
+/**
+ * Controller for script generation.
+ */
+@Controller('script-generator')
+export class ScriptGeneratorController {
+  constructor(
+    priva
