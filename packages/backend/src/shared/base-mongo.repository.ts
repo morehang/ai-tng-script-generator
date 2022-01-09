398b4a4af183eb2e842ca19d8b5d8a93e450aa8f
morehang <166088095+morehang@users.noreply.github.com>
2022-01-09 06:06:18 +0000

    Fix compile warning.

--- packages/backend/src/shared/base-mongo.repository.ts
+++ packages/backend/src/shared/base-mongo.repository.ts
@@ -18,3 +18,25 @@ export abstract class BaseMongoRepository {
    * Find single instance of data by id.
    * @param id Id of instance to find.
    */
+  public async findOneById(_id: any) {
+    return await this.collection.findOne({ _id });
+  }
+
+  /**
+   * Find all instances of stored data.
+   */
+  public async findAll(query?: any, projection?: any) {
+    return await this.collection
+      .find(query)
+      .project(projection)
+      .toArray();
+  }
+
+  /**
+   * Store instance.
+   * @param instance Instance to store.
+   */
+  public async save(instance: any) {
+    return this.collection.save(instance);
+  }
+}
---
+++ packages/backend/src/shared/shared.module.ts
@@ -0,0 +1,3 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '../config/config.module';
+import { MongoConnectionService } from './mongo-c
