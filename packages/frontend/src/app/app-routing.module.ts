54904272a32d942dc360c091da028b30c6e91902
morehang <166088095+morehang@users.noreply.github.com>
2022-03-30 18:43:17 +0000

    Organized code

--- packages/frontend/src/app/app-routing.module.ts
+++ packages/frontend/src/app/app-routing.module.ts
@@ -1,2 +1,7 @@
 import { NgModule } from '@angular/core';
-import { Routes, Rout
+import { Routes, RouterModule } from '@angular/router';
+import { AuthWithJwtPageComponent } from './auth-with-jwt-page/auth-with-jwt-page.component';
+import { HomePageComponent } from './home-page/home-page.component';
+
+/**
+ * SP
