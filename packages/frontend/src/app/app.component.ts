ce7d38d9497241b8fe31bc2bd5a21adbc04271ff
morehang <166088095+morehang@users.noreply.github.com>
2022-08-14 18:12:39 +0000

    version: post-release bump

--- packages/frontend/src/app/app.component.ts
+++ packages/frontend/src/app/app.component.ts
@@ -7,4 +7,4 @@ import { Component } from '@angular/core';
   selector: 'app-root',
   templateUrl: './app.component.html'
 })
-export class AppCo
+export class AppComponent {}
---
+++ packages/frontend/src/app/auth-interceptor.service.ts
@@ -0,0 +1,7 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent
+} from '@angular/common/http
