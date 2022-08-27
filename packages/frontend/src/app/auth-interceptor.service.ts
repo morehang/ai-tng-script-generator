6d6bc0d7bf52994f718011034f846bcaf65ee30d
morehang <166088095+morehang@users.noreply.github.com>
2022-08-27 14:37:50 +0000

    compatibility fix

--- packages/frontend/src/app/auth-interceptor.service.ts
+++ packages/frontend/src/app/auth-interceptor.service.ts
@@ -21,4 +21,10 @@ export class AuthInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     // Get JWT from local storage.
     const idToken = localStorage.getItem('id_token');
- 
+    if (idToken) {
+      const cloned = req.clone({
+        headers: req.headers.set('Authorization', `Bearer ${idToken}`)
+      });
+      return next.handle(cloned);
+    } else {
+      return next.hand
