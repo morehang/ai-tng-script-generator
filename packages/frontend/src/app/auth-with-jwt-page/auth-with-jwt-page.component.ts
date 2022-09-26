7d4f40b39f657431a14f84dfbfd94e013290fd8a
morehang <166088095+morehang@users.noreply.github.com>
2022-09-26 10:30:39 +0000

    Fixes #91

--- packages/frontend/src/app/auth-with-jwt-page/auth-with-jwt-page.component.ts
+++ packages/frontend/src/app/auth-with-jwt-page/auth-with-jwt-page.component.ts
@@ -10,4 +10,10 @@ import { delay } from '../delay.util';
   selector: 'app-auth-with-jwt-page',
   templateUrl: './auth-with-jwt-page.component.html'
 })
-export class A
+export class AuthWithJwtPageComponent implements OnInit {
+  constructor(private route: ActivatedRoute, private router: Router) {}
+
+  public isAuthenticated = false;
+
+  async ngOnInit() {
+    const jwt: string = this.route.snapshot.par
