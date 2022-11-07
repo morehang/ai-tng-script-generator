085cfa6e45d790c1c96d925a281767f46560e1da
morehang <166088095+morehang@users.noreply.github.com>
2022-11-07 19:27:47 +0000

    BEHNAM MARZJI

---
+++ packages/frontend/src/app/generator/generator.component.ts
@@ -0,0 +1,4 @@
+
+import { Component } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NotAuthenticatedAlertComponent } from '../not-authenticated-alert/not-authenticated-alert.component';
