4bbd2a86d189a280fe83fa25876e0e8653cd6f00
morehang <166088095+morehang@users.noreply.github.com>
2022-11-30 18:25:11 +0000

    67

--- packages/frontend/src/app/home-page/home-page.component.ts
+++ packages/frontend/src/app/home-page/home-page.component.ts
@@ -4,4 +4,9 @@ import { Component } from '@angular/core';
  * Home page of Script Generator.
  */
 @Component({
-  selector: 'app-home-pa
+  selector: 'app-home-page',
+  templateUrl: './home-page.component.html'
+})
+export class HomePageComponent {
+  constructor() {}
+}
