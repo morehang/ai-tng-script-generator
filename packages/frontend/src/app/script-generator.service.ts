10141ad07820278d7e6daf109cc93b00e4477214
morehang <166088095+morehang@users.noreply.github.com>
2022-12-06 05:55:36 +0000

    make it compile

--- packages/frontend/src/app/script-generator.service.ts
+++ packages/frontend/src/app/script-generator.service.ts
@@ -3,4 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment';
 
 /**
- * 
+ * REST client for Script Generator Services.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class ScriptGeneratorService {
+  constructor(private http: HttpClient) {}
+
+  /**
+   * Base url of service to 
