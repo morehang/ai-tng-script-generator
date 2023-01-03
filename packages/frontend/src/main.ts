b446bb9d633df3ff8b6b62b7027da2bc4ffcb100
morehang <166088095+morehang@users.noreply.github.com>
2023-01-03 09:25:54 +0000

    usb: Prevent dead ports when xhci is not enabled

--- packages/frontend/src/main.ts
+++ packages/frontend/src/main.ts
@@ -1,2 +1,11 @@
 import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-brow
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapMo
