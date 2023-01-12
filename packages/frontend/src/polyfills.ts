2cce79da9f4edc274d9622de15086a2adfb10e27
morehang <166088095+morehang@users.noreply.github.com>
2023-01-12 07:21:35 +0000

    Update tags.md

--- packages/frontend/src/polyfills.ts
+++ packages/frontend/src/polyfills.ts
@@ -24,4 +24,9 @@
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
- * St
+ * Standard animation support in Angular DOES NOT require any polyfills (as of Angular 6.0).
+ */
+// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
+
+/**
+ * By default, zone.js will patch all po
