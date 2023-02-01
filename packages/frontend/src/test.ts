ca9a58e58be9dded9672c703f15b47750d1ea1fb
morehang <166088095+morehang@users.noreply.github.com>
2023-02-01 16:24:49 +0000

    Update ReadMe.

--- packages/frontend/src/test.ts
+++ packages/frontend/src/test.ts
@@ -3,3 +3,11 @@
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+declare const require: any;
+
+// First, initialize the Angular testing environment.
+ge
