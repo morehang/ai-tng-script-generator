5d9f2cfbe47da40c49d9909f9cd1dcc03867fa5c
morehang <166088095+morehang@users.noreply.github.com>
2023-03-14 05:50:27 +0000

    mei: bus: fix possible boundaries violation

---
+++ packages/tng_scriptgen_flutter_app/android/app/src/main/java/com/ragres/tng_scriptgen_flutter_app/MainActivity.java
@@ -0,0 +1,7 @@
+package com.ragres.tng_scriptgen_flutter_app;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public cla
