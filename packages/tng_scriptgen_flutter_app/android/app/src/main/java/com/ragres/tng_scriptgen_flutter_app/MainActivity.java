d62c8e72141cc48e28d82b70ce77de174cb239e9
morehang <166088095+morehang@users.noreply.github.com>
2023-03-14 08:10:04 +0000

    validation fix

--- packages/tng_scriptgen_flutter_app/android/app/src/main/java/com/ragres/tng_scriptgen_flutter_app/MainActivity.java
+++ packages/tng_scriptgen_flutter_app/android/app/src/main/java/com/ragres/tng_scriptgen_flutter_app/MainActivity.java
@@ -4,4 +4,8 @@ import android.os.Bundle;
 import io.flutter.app.FlutterActivity;
 import io.flutter.plugins.GeneratedPluginRegistrant;
 
-public cla
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(thi
