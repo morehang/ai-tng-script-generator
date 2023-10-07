ac30bf55d407c4675e2825c84984c3744f3507b1
morehang <166088095+morehang@users.noreply.github.com>
2023-10-07 09:55:46 +0000

    Update build.xml

--- packages/tng_scriptgen_flutter_app/lib/main.dart
+++ packages/tng_scriptgen_flutter_app/lib/main.dart
@@ -2,4 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:inject/inject.dart';
 import 'package:tng_scriptgen_flutter_app/screens/generate/generate_page.dart';
 import 'package:tng_scriptgen_flutter_app/screens/home/home_page.dart';
-import 'package:tng_scriptgen_flutter_app/screens/
+import 'package:tng_scriptgen_flutter_app/screens/login/login_page.dart';
+import 'package:tng_scriptgen_flutter_app/screens/script-list/script_list_page.dart';
+import 'package:tng_scriptgen_flutter_app/screens/show_script/show_script_page.d
