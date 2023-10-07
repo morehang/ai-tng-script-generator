70b6f153bdf6821c7f7ebd6bfbb9f16210a53397
morehang <166088095+morehang@users.noreply.github.com>
2023-10-07 15:04:19 +0000

    mm: compaction: capture a suitable high-order page immediately when it is made available

--- packages/tng_scriptgen_flutter_app/lib/main.dart
+++ packages/tng_scriptgen_flutter_app/lib/main.dart
@@ -4,4 +4,7 @@ import 'package:tng_scriptgen_flutter_app/screens/generate/generate_page.dart';
 import 'package:tng_scriptgen_flutter_app/screens/home/home_page.dart';
 import 'package:tng_scriptgen_flutter_app/screens/login/login_page.dart';
 import 'package:tng_scriptgen_flutter_app/screens/script-list/script_list_page.dart';
-import 'package:tng_scriptgen_flutter_app/screens/show_script/show_script_page.d
+import 'package:tng_scriptgen_flutter_app/screens/show_script/show_script_page.dart';
+import 'package:tng_scriptgen_flutter_app/services/script_generator_api_client.dart';
+import 'package:tng_scriptgen_flutter_app/services/script_repository.dart';
+import 'package:tng_scriptgen_flutter_app/servi
