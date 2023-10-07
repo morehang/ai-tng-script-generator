33394e4e344936a838e047e27975ba125cae50ab
morehang <166088095+morehang@users.noreply.github.com>
2023-10-07 19:38:04 +0000

    Fixes #45

--- packages/tng_scriptgen_flutter_app/lib/main.dart
+++ packages/tng_scriptgen_flutter_app/lib/main.dart
@@ -7,4 +7,13 @@ import 'package:tng_scriptgen_flutter_app/screens/script-list/script_list_page.d
 import 'package:tng_scriptgen_flutter_app/screens/show_script/show_script_page.dart';
 import 'package:tng_scriptgen_flutter_app/services/script_generator_api_client.dart';
 import 'package:tng_scriptgen_flutter_app/services/script_repository.dart';
-import 'package:tng_scriptgen_flutter_app/servi
+import 'package:tng_scriptgen_flutter_app/services/script_sync_service.dart';
+import 'main.inject.dart' as g;
+
+@Injector()
+abstract class Main {
+  @provide
+  MyApp get app;
+  static Future<Main> create(
+      ) async {
+    return await g.Main$Injector.create();
