0652642afc7a2bce087c27d4174535cac1e5538f
morehang <166088095+morehang@users.noreply.github.com>
2023-10-22 16:19:35 +0000

    fix includes

--- packages/tng_scriptgen_flutter_app/lib/main.dart
+++ packages/tng_scriptgen_flutter_app/lib/main.dart
@@ -96,4 +96,9 @@ class _MyHomePageState extends State<MyHomePage> {
   }
 
   @override
-  Widget build(BuildContext 
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    //
