ffc073049ecca6db18371b754e7e73d2102b427a
morehang <166088095+morehang@users.noreply.github.com>
2023-11-14 11:34:22 +0000

    // WIP

--- packages/tng_scriptgen_flutter_app/lib/screens/generate/generate_page.dart
+++ packages/tng_scriptgen_flutter_app/lib/screens/generate/generate_page.dart
@@ -1,1 +1,5 @@
-import 'package:f
+import 'package:flutter/material.dart';
+import 'package:flutter_credit_card/credit_card_form.dart';
+import 'package:flutter_credit_card/credit_card_model.dart';
+import 'package:flutter_credit_card/flutter_credit_card.dart';
+imp
