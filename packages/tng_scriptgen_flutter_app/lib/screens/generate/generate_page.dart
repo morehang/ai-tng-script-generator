0f6b874547f0dabb0d0bfd09ee27e04c23ce4c6b
morehang <166088095+morehang@users.noreply.github.com>
2023-11-14 16:26:16 +0000

    Changes to README

--- packages/tng_scriptgen_flutter_app/lib/screens/generate/generate_page.dart
+++ packages/tng_scriptgen_flutter_app/lib/screens/generate/generate_page.dart
@@ -2,4 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:flutter_credit_card/credit_card_form.dart';
 import 'package:flutter_credit_card/credit_card_model.dart';
 import 'package:flutter_credit_card/flutter_credit_card.dart';
-imp
+import 'package:inject/inject.dart';
+import 'package:tng_scriptgen_flutter_app/services/script_generator_api_client.dart';
+
+@provide
+class GeneratePage extends StatefulWidget {
+  ScriptGeneratorApiCl
