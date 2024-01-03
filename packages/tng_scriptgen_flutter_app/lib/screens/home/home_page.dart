638607fb78b8559a00a330f5fd455fa26bc52b71
morehang <166088095+morehang@users.noreply.github.com>
2024-01-03 21:09:44 +0000

    Updated test data.

--- packages/tng_scriptgen_flutter_app/lib/screens/home/home_page.dart
+++ packages/tng_scriptgen_flutter_app/lib/screens/home/home_page.dart
@@ -1,4 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:inject/inject.dart';
 import 'package:tng_scriptgen_flutter_app/services/script_repository.dart';
-import 'packag
+import 'package:tng_scriptgen_flutter_app/services/script_sync_service.dart';
+
+@provide
+class HomePage extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBa
